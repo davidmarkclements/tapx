31a7eb21e8e174a4901cbe39fcdbc32eeabce3a1
David Mark Clements <david.mark.clements@gmail.com>
2021-02-03 23:57:14 +0100

    drop cov support for node 12

--- readme.md
+++ readme.md
@@ -8,13 +8,16 @@ Supports the following:
 import { test } from 'tapx'
 ```
 
-This module (at least the version 0.x.x) line will be deprecated when https://github.com/tapjs/node-tap/pull/668/files lands.
-
 It also overrides the `tap` test runner so that the TAP_NO_ESM environment variable is set. This (counterintuitively) allows node-tap to work with native ESM modules - because it suppresses faux-ESM support.
 
 `nyc` currently does not work with native ESM. `tapx` uses `c8` to provide coverage instead. 
 
+Due to some coverage bugs on Node 12 or below, coverage is not generated at all for Node 12 or below. 
+Tests will still run. Use Node 14 to check coverage.
+
 An additional `cov` command is also provided, which is the equivalent to `tap --coverage-report=HTML`.
+
+This module (at least the version 0.x.x) line will be deprecated when https://github.com/tapjs/node-tap/pull/668/files lands and when node-tap has coverage support for native ESM.
 
 ## License
 
--- tap.cjs
+++ tap.cjs
@@ -6,12 +6,20 @@ const tap = dirname(require.resolve('tap/package.json'))
 const c8 = dirname(require.resolve('c8/package.json'))
 const { bin } = require('tap/package.json')
 const c8Path = join(c8, require('c8/package.json').bin.c8)
-const cov = (process.argv.indexOf('--100') > -1) ? [
-  '--check-coverage', '--lines 100', '--functions 100', '--branches 100'
-] : []
+const cov = (process.argv.indexOf('--100') > -1)
+  ? ['--check-coverage', '--lines 100', '--functions 100', '--branches 100']
+  : []
 
-spawn(
-  process.execPath,
-  [c8Path, ...cov, 'node', join(tap, bin.tap), ...process.argv.slice(2), '--no-cov'],
-  { stdio: 'inherit', env: { ...process.env, TAP_NO_ESM: 1 } }
-)
+if (Number(process.versions.node.split('.').shift()) < 14) {
+  spawn(
+    process.execPath,
+    [join(tap, bin.tap), ...process.argv.slice(2), '--no-cov'],
+    { stdio: 'inherit', env: { ...process.env, TAP_NO_ESM: 1 } }
+  )
+} else {
+  spawn(
+    process.execPath,
+    [c8Path, ...cov, 'node', join(tap, bin.tap), ...process.argv.slice(2), '--no-cov'],
+    { stdio: 'inherit', env: { ...process.env, TAP_NO_ESM: 1 } }
+  )
+}
