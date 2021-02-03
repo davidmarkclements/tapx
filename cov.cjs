d320408123ef07dc243b9c6b8cc34be5740ad91d
David Mark Clements <david.mark.clements@gmail.com>
2021-02-03 23:28:16 +0100

    bug fix

--- cov.cjs
+++ cov.cjs
@@ -1,15 +1,16 @@
 #!/usr/bin/env node
 'use strict'
 const { spawnSync } = require('child_process')
-const { join } = require('path')
+const { join, dirname } = require('path')
+const c8 = dirname(require.resolve('c8/package.json'))
+const { bin } = require('c8/package.json')
 const open = require('open')
 const cwd = process.cwd()
 spawnSync(
-  join(__dirname, 'node_modules', '.bin', 'c8'), 
-  [ 'report', '-r', 'html' ], 
+  process.execPath,
+  [join(c8, bin.c8), 'report', '-r', 'html'],
   { stdio: 'inherit' }
 )
 if (process.argv.indexOf('--no-open') === -1) {
   open(join(cwd, 'coverage', 'index.html'))
 }
-
--- tap.cjs
+++ tap.cjs
@@ -3,13 +3,15 @@
 const { spawn } = require('child_process')
 const { join, dirname } = require('path')
 const tap = dirname(require.resolve('tap/package.json'))
+const c8 = dirname(require.resolve('c8/package.json'))
 const { bin } = require('tap/package.json')
+const c8Path = join(c8, require('c8/package.json').bin.c8)
 const cov = (process.argv.indexOf('--100') > -1) ? [
   '--check-coverage', '--lines 100', '--functions 100', '--branches 100'
 ] : []
 
 spawn(
-  join(__dirname, 'node_modules', '.bin', 'c8'),
-  [ ...cov, 'node', join(tap, bin.tap), ...process.argv.slice(2), '--no-cov' ],
+  process.execPath,
+  [c8Path, ...cov, 'node', join(tap, bin.tap), ...process.argv.slice(2), '--no-cov'],
   { stdio: 'inherit', env: { ...process.env, TAP_NO_ESM: 1 } }
 )
